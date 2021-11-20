31dfe270b5974956d9e5162928d93dc4903b24bf
Bargavi19 <88387622+Bargavi19@users.noreply.github.com>
2021-11-20 22:38:40 +0000

    quadrilateral_1st_part_commits (#37)

--- shapes/lib/quadrilateral.rb
+++ shapes/lib/quadrilateral.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 def quadrilateral(a, b, c, d)
-  # WRITE THIS CODE
+  angles = [a, b, c, d].sort
+  if angles[0] <= 0 || angles.sum !=360
+    raise QuadrilateralError
+  end
+  case angles.uniq.size
+  when 1 then [:square, :rectangle]
+  when 2 then [:parallelogram, :rhombus]
+  when 4 then [:quadrilateral]
+  end
 end
 
 # Error class used in invalid_quadrilateral_spec. No need to change this code.
