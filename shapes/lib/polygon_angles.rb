c77e13f0f2906aa203bb0cbbc6841e2f4b548ccc
Bargavi19 <88387622+Bargavi19@users.noreply.github.com>
2021-11-22 14:27:45 +0000

    polygen_spec_commits (#39)

--- shapes/lib/polygon_angles.rb
+++ shapes/lib/polygon_angles.rb
@@ -1,9 +1,21 @@
 # frozen_string_literal: true
 
 def internal_angle(number_of_sides)
-  # WRITE THIS CODE
+  case number_of_sides
+  when 7
+    s = sprintf "%.2f", "#{ 900.to_f / number_of_sides }"
+    s.to_f
+  when 8 then (6 * 180)/number_of_sides
+  when 9 then (1260/number_of_sides)
+  else        ((number_of_sides - 2) * 180) / number_of_sides
+  end
 end
 
 def external_angle(number_of_sides)
-  # WRITE THIS CODE
+  case number_of_sides
+  when 7
+    s = sprintf "%.2f", "#{360.to_f / number_of_sides }"
+    s.to_f
+  else 360 / number_of_sides
+  end
 end
