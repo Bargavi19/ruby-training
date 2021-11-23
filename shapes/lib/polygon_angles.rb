7f37a9151462f64d78dc9d2f3f713fba53b4fbb1
Bargavi19 <bargavi.muthusubramanian@globacap.com>
2021-11-23 10:58:00 +0000

    adding_comments_for_quadrilateral_polygen

--- shapes/lib/polygon_angles.rb
+++ shapes/lib/polygon_angles.rb
@@ -1,10 +1,17 @@
 # frozen_string_literal: true
 
 def internal_angle(number_of_sides)
+  # Below code calculates the
+  # interior angle of a regular heptagon
+  # interior angle of a regular octagon
+  # interior angle of a regular nonagon
+  # interior angle of a regular pentagon
+  # interior angle of a regular hexagon
+  # interior angle of a regular decagon
   case number_of_sides
   when 7
-    s = sprintf "%.2f", "#{ 900.to_f / number_of_sides }"
-    s.to_f
+    s = sprintf "%.2f", "#{ 900.to_f / number_of_sides }" # this will give string as the answer with 2 decimals
+    s.to_f # converting the above string to float
   when 8 then (6 * 180)/number_of_sides
   when 9 then (1260/number_of_sides)
   else        ((number_of_sides - 2) * 180) / number_of_sides
@@ -12,6 +19,13 @@ def internal_angle(number_of_sides)
 end
 
 def external_angle(number_of_sides)
+  # Below code calculates the
+  # exterior angle of a regular heptagon
+  # exterior angle of a regular pentagon
+  # exterior angle of a regular hexagon
+  # exterior angle of a regular octagon
+  # exterior angle of a regular nonagon
+  # exterior angle of a regular decagon
   case number_of_sides
   when 7
     s = sprintf "%.2f", "#{360.to_f / number_of_sides }"
--- shapes/lib/quadrilateral.rb
+++ shapes/lib/quadrilateral.rb
@@ -1,20 +1,29 @@
 # frozen_string_literal: true
 
 def quadrilateral(a, b, c, d)
+  # Below code will check errors for
+  # quadrilaterals with angles of 0
+  # quadrilaterals with negative angles
+  # illegal quadrilaterals that are not constructable
   angles = [a, b, c, d].sort
   if angles[0] <= 0 || angles.sum !=360
     raise QuadrilateralError
   end
-  case angles.uniq.size
+  # Below code will check for
+  # square or rectangle when all angles are the same
+  # parallelogram or rhombus when there are 2 pairs of equal angles
+  # quadrilateral when no angles are equal
+  # quadrilateral when there is not 2 pairs of equal angles
+  case angles.uniq.size # uniq method of array will give new array by removing duplicate elements
   when 1 then [:square, :rectangle]
   when 2 .. 4
     a=[a, b, c, d]
     first = a.first
     o = first + a[2]
     t = first + a[3]
-    if [o, t].include? 180
+    if [o, t].include? 180  #Parallelogram/rhombus will give the sum of adjacent angles as 180
       [:parallelogram, :rhombus]
-    else
+    else #quadrilateral won't give the sum of adjacent angles as 180
        [:quadrilateral]
     end
   end
