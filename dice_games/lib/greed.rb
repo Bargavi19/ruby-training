9b6ed407046e4f3d3f0e4dc0872218705b4e16e7
Bargavi19 <bargavi.muthusubramanian@globacap.com>
2021-11-27 22:28:57 +0000

    added_conditions_for_array_inputs_with_length_3

--- dice_games/lib/greed.rb
+++ dice_games/lib/greed.rb
@@ -11,6 +11,15 @@ class Greed
        return 50
       elsif a == [1]
        return 100
+      elsif a.count(1) == 2 && a.count(5) == 2
+        return 300
+      elsif a.length == 3
+        b = a.uniq
+        if b[0] != 1
+        return b[0] * 100
+        else
+        return 1000
+        end
       else
         return 0
       end
