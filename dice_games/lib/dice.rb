cf6f3c3cd9bd3d203b75d9c346ae8932b5bd12a1
Bargavi19 <88387622+Bargavi19@users.noreply.github.com>
2021-11-27 15:30:12 +0000

    method_roll_returns_same_size_as_requested (#46)

--- dice_games/lib/dice.rb
+++ dice_games/lib/dice.rb
@@ -2,7 +2,12 @@
 
 class Dice
   def roll (n = 1)
-    @values =[1]
+    @values = []  # declaring instance variable as array. If we didn't, then we cannot use this as array variable
+    i = 0
+    while(i < n)
+      @values[i] = rand(1..6)
+      i += 1
+    end
   end
   def values
     @values
