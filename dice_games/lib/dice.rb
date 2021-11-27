9cc0d4812950d9ddb400ba4e1844dd6e59708f5d
Bargavi19 <bargavi.muthusubramanian@globacap.com>
2021-11-27 16:14:35 +0000

    dice_games_optimization

--- dice_games/lib/dice.rb
+++ dice_games/lib/dice.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 class Dice
+  attr_reader :values # same results as def values defined previously
   def roll (n = 1)
-    @values = []  # declaring instance variable as array. If we didn't, then we cannot use this as array variable
+    @values = []
+    # using instance variable so that it can be used across methods
+    # declaring instance variable as array. If we didn't, then we cannot use this as array variable
     i = 0
     while(i < n)
       @values[i] = rand(1..6)
       i += 1
     end
-  end
-  def values
-    @values
   end
 end
