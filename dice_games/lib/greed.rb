71bb77c5fcdbb605b766cadcfe46e48a29508484
Bargavi19 <88387622+Bargavi19@users.noreply.github.com>
2021-11-28 15:55:06 +0000

    refactored_for_simple_low_scoring_hands (#54)

--- dice_games/lib/greed.rb
+++ dice_games/lib/greed.rb
@@ -2,47 +2,30 @@
 
 class Greed
   def score (a)
-    if a.is_a?(Array)
-      if a == [5]
-       return 50
-      elsif a == [1]
-       return 100
-      elsif a.count(1) == 2 && a.count(5) == 2
-        return 300
-      elsif a.length == 3
-        b = a.uniq
-        if b[0] != 1
-        return b[0] * 100
-        else
-        return 1000
-        end
-      elsif a.length == 5
-        if a.include?(6) && a.count(6) == 1
-          if a.include?(1)
-          return 1050
-          else return 50
-          end
-        elsif a.include?(1)
-          return 1200 if a.count(1) ==5
-          if a.count(1) == 1
-          return 150
-          else
-            if a.count(4) == 3
-            return 600
-            else
-              return 800
-            end
-          end
-        elsif a.count(4) == 4 && a.count(5) == 1
-          return 450
-        elsif a.count(5) == 4 && a.count(4) == 1
-          return 550
-        end
+    point_scored = 0
+    return 0 if a == []
+    if a[1] == a[0] && a[2] == a[0]
+      if a[0] == 1
+        point_scored += 1000
       else
-        return 0
+        point_scored += a[0] * 100
+      end
+      a.shift
+      a.shift
+      a.shift
+    end
+    a. each do |die|
+      if die == 1
+        point_scored += 100
+      elsif die == 5
+        point_scored +=50
       end
     end
+    point_scored
   end
 end
-class GreedError < StandardError
-end
+
+  class GreedError < StandardError
+
+  end
+
