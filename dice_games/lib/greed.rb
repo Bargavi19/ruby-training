7d649c0f95f1514aff320a77ab00e6111d8efd8b
Bargavi19 <bargavi.muthusubramanian@globacap.com>
2021-11-28 16:30:01 +0000

    refactoring_for_medium_high_scoring_hands

--- dice_games/lib/greed.rb
+++ dice_games/lib/greed.rb
@@ -4,15 +4,26 @@ class Greed
   def score (a)
     point_scored = 0
     return 0 if a == []
+    a = a.sort
     if a[1] == a[0] && a[2] == a[0]
       if a[0] == 1
         point_scored += 1000
       else
         point_scored += a[0] * 100
       end
-      a.shift
-      a.shift
-      a.shift
+      3.times { a.shift }
+    end
+    if a.count(5) > 3
+      point_scored += 5 * 100
+      4.times { a.shift }
+    end
+    if a.count(4) == 3 || a.count(6) == 3
+      if a.include?(4)
+        point_scored += 4 * 100
+      else
+        point_scored += 6 * 100
+      end
+      3.times { a.pop }
     end
     a. each do |die|
       if die == 1
@@ -26,6 +37,5 @@ class Greed
 end
 
   class GreedError < StandardError
-
   end
 
