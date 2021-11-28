e405937b9b63b614ac9461b8bf945ca34d35a73d
Bargavi19 <88387622+Bargavi19@users.noreply.github.com>
2021-11-28 13:35:51 +0000

    Dice games 2ndtime optimization (#48)
    * optimizing_dice_games_2nd_time
    
    * removed_the_argument_roll

--- dice_games/lib/dice.rb
+++ dice_games/lib/dice.rb
@@ -5,11 +5,7 @@ class Dice
   def roll (n = 1)
     @values = []
     # using instance variable so that it can be used across methods
-    # declaring instance variable as array. If we didn't, then we cannot use this as array variable
-    i = 0
-    while(i < n)
-      @values[i] = rand(1..6)
-      i += 1
-    end
+    # declaring instance variable as array. If we didn't, then it will return as array
+    n.times { @values << rand(1..6) }
   end
 end
