36c9c94b900db3f937889a3a7c68a42887efe987
Bargavi19 <bargavi.muthusubramanian@globacap.com>
2021-11-29 19:34:18 +0000

    optimized_the_code_for_greed_spec

--- dice_games/lib/greed.rb
+++ dice_games/lib/greed.rb
@@ -1,41 +1,32 @@
 # frozen_string_literal: true
 
 class Greed
-  def score (a)
-    point_scored = 0
-    return 0 if a == []
-    a = a.sort
-    if a[1] == a[0] && a[2] == a[0]
-      if a[0] == 1
-        point_scored += 1000
-      else
-        point_scored += a[0] * 100
-      end
-      3.times { a.shift }
+  def score (a = nil)
+    if a == nil
+      raise GreedError
     end
-    if a.count(5) > 3
-      point_scored += 5 * 100
-      4.times { a.shift }
+     point_scored = 0 # score is set to 0 to start off so if no dice, no score
+    if (a.count(1)/3) == 1 # setting the 1000 1,1,1 rule
+      point_scored += 1000
+      3.times { a.shift } # deleting first 3 1's in the array so that it cannot be recalculated when we calculate for the single 1
     end
-    if a.count(4) == 3 || a.count(6) == 3
-      if a.include?(4)
-        point_scored += 4 * 100
-      else
-        point_scored += 6 * 100
-      end
-      3.times { a.pop }
+    [2, 3, 4, 5, 6].each do |num| # set the other triples here
+       if (a.count(num)/3) == 1
+         point_scored += num * 100
+       end
     end
-    a. each do |die|
+    3.times { a.pop } if point_scored == 500 # deleting last 3 5's from the array. So that it cannot be counted for single 5 calculation
+    a. each do |die| # taking care of the single 5s and 1s here
       if die == 1
         point_scored += 100
       elsif die == 5
         point_scored +=50
       end
     end
-    point_scored
+     point_scored
   end
 end
 
-  class GreedError < StandardError
+  class GreedError < ArgumentError
   end
 
