d1a1e6594cf9c6aba23a32b47451ac07535028ff
Bargavi19 <bargavi.muthusubramanian@globacap.com>
2021-12-04 22:28:26 +0000

    can sing a typical verse

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Bottles
+  def verse(number_of_bottles)
+    a1 = "#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.\nTake one down and pass it around, #{number_of_bottles - 1} bottles of beer on the wall.\n"
+  end
 end
