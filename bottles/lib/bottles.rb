d2aec655d34a725903bdfc79d3f0c727d0e46a41
Bargavi19 <bargavi.muthusubramanian@globacap.com>
2021-12-05 15:04:08 +0000

    can sing about 1 bottle and 2 bottles

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -2,6 +2,11 @@
 
 class Bottles
   def verse(number_of_bottles)
-    a1 = "#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.\nTake one down and pass it around, #{number_of_bottles - 1} bottles of beer on the wall.\n"
+    # a1 = "#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.\nTake one down and pass it around, #{number_of_bottles - 1} bottles of beer on the wall.\n"
+    if number_of_bottles == 1
+      a1="1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+    else
+      a1="#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.\nTake one down and pass it around, #{number_of_bottles - 1} bottles of beer on the wall.\n"
+    end
   end
 end
