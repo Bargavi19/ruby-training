285c4196a4033a5264668b6a78caa6ea939e866d
Bargavi19 <88387622+Bargavi19@users.noreply.github.com>
2021-12-05 21:40:08 +0000

    Can sing typical verse (#61)
    * can sing a typical verse
    
    * can sing another typical verse
    
    * can sing about 1 bottle and 2 bottles
    
    * can sing about 2 bottles and 1 bottle

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,4 +1,25 @@
 # frozen_string_literal: true
 
 class Bottles
+  attr_reader :a0, :a4,:a1, :a3, :a2
+  def verse(number_of_bottles)
+    @a0 ="#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer."
+    @a1 = "Take one down and pass it around, #{number_of_bottles - 1} bottles of beer on the wall."
+    if number_of_bottles == 2
+      verse2
+    elsif number_of_bottles == 1
+      verse1
+    else
+      expected = [a0, a1].join("\n") + "\n"
+    end
+  end
+  def verse2
+    @a2 ="Take one down and pass it around, 1 bottle of beer on the wall."
+    expected = [a0, a2].join("\n") + "\n"
+  end
+  def verse1
+    @a3 ="1 bottle of beer on the wall, 1 bottle of beer."
+    @a4 ="Take it down and pass it around, no more bottles of beer on the wall."
+    expected = [a3, a4].join("\n") + "\n"
+  end
 end
