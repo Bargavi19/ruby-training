46c7c3bef3d95757bad87534022a01d51a7b75c2
Bargavi19 <88387622+Bargavi19@users.noreply.github.com>
2021-12-07 20:02:35 +0000

    Can string a few verses together (#62)
    * can sing about no more bottles
    
    * can string a few verses together

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Bottles
-  attr_reader :a0, :a4,:a1, :a3, :a2
+  attr_reader :a0, :a1,:a2, :a3, :a4, :a5, :a6
   def verse(number_of_bottles)
     @a0 ="#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer."
     @a1 = "Take one down and pass it around, #{number_of_bottles - 1} bottles of beer on the wall."
@@ -9,6 +9,8 @@ class Bottles
       verse2
     elsif number_of_bottles == 1
       verse1
+    elsif number_of_bottles == 0
+      verse0
     else
       expected = [a0, a1].join("\n") + "\n"
     end
@@ -21,5 +23,17 @@ class Bottles
     @a3 ="1 bottle of beer on the wall, 1 bottle of beer."
     @a4 ="Take it down and pass it around, no more bottles of beer on the wall."
     expected = [a3, a4].join("\n") + "\n"
+  end
+  def verse0
+    @a5 = "No more bottles of beer on the wall, no more bottles of beer."
+    @a6 = "Go to the store and buy some more, 99 bottles of beer on the wall."
+    expected =  [a5, a6].join("\n") + "\n"
+  end
+  def verses(start_value, end_value)
+    expected = []
+    (end_value..start_value).each do |n|
+      expected << verse(n) + "\n"
+    end
+    expected1 = expected.sort! .reverse.join("")
   end
 end
