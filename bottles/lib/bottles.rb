0ad15e975fcb9e9063e8a860c76031003a9e5b3b
Bargavi19 <88387622+Bargavi19@users.noreply.github.com>
2021-12-08 11:16:35 +0000

    Can sing the whole song (#63)
    * can sing the whole song
    
    * Declaring my own methods as private
    
    * added the word private for private methods
    
    * removed the unwanted method return variables

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -6,34 +6,40 @@ class Bottles
     @a0 ="#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer."
     @a1 = "Take one down and pass it around, #{number_of_bottles - 1} bottles of beer on the wall."
     if number_of_bottles == 2
-      verse2
+      private_verse2
     elsif number_of_bottles == 1
-      verse1
+      private_verse1
     elsif number_of_bottles == 0
-      verse0
+      private_verse0
     else
-      expected = [a0, a1].join("\n") + "\n"
+      [a0, a1].join("\n") + "\n"
     end
-  end
-  def verse2
-    @a2 ="Take one down and pass it around, 1 bottle of beer on the wall."
-    expected = [a0, a2].join("\n") + "\n"
-  end
-  def verse1
-    @a3 ="1 bottle of beer on the wall, 1 bottle of beer."
-    @a4 ="Take it down and pass it around, no more bottles of beer on the wall."
-    expected = [a3, a4].join("\n") + "\n"
-  end
-  def verse0
-    @a5 = "No more bottles of beer on the wall, no more bottles of beer."
-    @a6 = "Go to the store and buy some more, 99 bottles of beer on the wall."
-    expected =  [a5, a6].join("\n") + "\n"
   end
   def verses(start_value, end_value)
     expected = []
     (end_value..start_value).each do |n|
       expected << verse(n) + "\n"
     end
-    expected1 = expected.sort! .reverse.join("")
+    expected.sort! .reverse.join("")
+  end
+  def sing
+    verses(99, 0)
+  end
+
+  private
+
+  def private_verse2
+    @a2 ="Take one down and pass it around, 1 bottle of beer on the wall."
+    [a0, a2].join("\n") + "\n"
+  end
+  def private_verse1
+    @a3 ="1 bottle of beer on the wall, 1 bottle of beer."
+    @a4 ="Take it down and pass it around, no more bottles of beer on the wall."
+    [a3, a4].join("\n") + "\n"
+  end
+  def private_verse0
+    @a5 = "No more bottles of beer on the wall, no more bottles of beer."
+    @a6 = "Go to the store and buy some more, 99 bottles of beer on the wall."
+    [a5, a6].join("\n") + "\n"
   end
 end
