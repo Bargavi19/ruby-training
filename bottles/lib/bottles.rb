d7b360413188d04de1621409891a51c21bc48259
Bargavi19 <bargavi.muthusubramanian@globacap.com>
2021-12-08 10:55:21 +0000

    added the word private for private methods

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -6,11 +6,11 @@ class Bottles
     @a0 ="#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer."
     @a1 = "Take one down and pass it around, #{number_of_bottles - 1} bottles of beer on the wall."
     if number_of_bottles == 2
-      verse2
+      private_verse2
     elsif number_of_bottles == 1
-      verse1
+      private_verse1
     elsif number_of_bottles == 0
-      verse0
+      private_verse0
     else
       expected = [a0, a1].join("\n") + "\n"
     end
@@ -25,19 +25,19 @@ class Bottles
   def sing
     verses(99, 0)
   end
-  
+
   private
 
-  def verse2
+  def private_verse2
     @a2 ="Take one down and pass it around, 1 bottle of beer on the wall."
     expected = [a0, a2].join("\n") + "\n"
   end
-  def verse1
+  def private_verse1
     @a3 ="1 bottle of beer on the wall, 1 bottle of beer."
     @a4 ="Take it down and pass it around, no more bottles of beer on the wall."
     expected = [a3, a4].join("\n") + "\n"
   end
-  def verse0
+  def private_verse0
     @a5 = "No more bottles of beer on the wall, no more bottles of beer."
     @a6 = "Go to the store and buy some more, 99 bottles of beer on the wall."
     expected =  [a5, a6].join("\n") + "\n"
