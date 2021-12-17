c7c359d9bd955755ab78e7dbac9fdfb2b544afb2
Bargavi19 <bargavi.muthusubramanian@globacap.com>
2021-12-17 18:00:07 +0000

    method returns digits 0,6,8

--- lcd/lib/lcd.rb
+++ lcd/lib/lcd.rb
@@ -1,5 +1,29 @@
 # frozen_string_literal: true
 
 class LCD
-  # Write your code in here
+  attr_reader :digit
+  def initialize(number)
+    @digit = number
+  end
+  def render
+    if digit == 0
+      <<~SQUIGGLY_HEREDOC
+         _ 
+        | |
+        |_|
+      SQUIGGLY_HEREDOC
+    elsif digit == 6
+      <<~SQUIGGLY_HEREDOC
+         _ 
+        |_ 
+        |_|
+      SQUIGGLY_HEREDOC
+    elsif digit == 8
+    <<~SQUIGGLY_HEREDOC
+         _ 
+        |_|
+        |_|
+    SQUIGGLY_HEREDOC
+    end
+  end
 end
