562879ef19ba7c19411e37ecda0829822d6e349f
Bargavi19 <bargavi.muthusubramanian@globacap.com>
2021-12-22 14:15:57 +0000

    renders a 8 test passed

--- lcd/lib/lcd.rb
+++ lcd/lib/lcd.rb
@@ -7,7 +7,8 @@ class LCD
     @lcdStates = %w[HORIZONTAL VERTICAL VERTICAL]
     @lcdDisplayData = {
       0 => [1, 3, 4],
-      6 => [1, 2, 4]
+      6 => [1, 2, 4],
+      8 => [1, 4, 4]
     }
   end
 
