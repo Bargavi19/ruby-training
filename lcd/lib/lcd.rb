5d2097e012932749985bfeb92222446a20573ca5
Bargavi19 <bargavi.muthusubramanian@globacap.com>
2021-12-22 18:57:22 +0000

    visualised digits from 0 to 9

--- lcd/lib/lcd.rb
+++ lcd/lib/lcd.rb
@@ -7,20 +7,32 @@ class LCD
     @lcdStates = %w[HORIZONTAL VERTICAL VERTICAL]
     @lcdDisplayData = {
       0 => [1, 3, 4],
+      1 => [0, 1, 1],
+      2 => [1, 5, 2],
+      3 => [1, 5, 5],
+      4 => [0, 4, 1],
+      5 => [1, 2, 5],
       6 => [1, 2, 4],
-      8 => [1, 4, 4]
+      7 => [1, 1, 1],
+      8 => [1, 4, 4],
+      9 => [1, 4, 5]
     }
   end
 
   def render
-    e = lcdStates.each_with_index.map do |w, index|
-             if w ==  "HORIZONTAL"
-                horizontal_segment(lcdDisplayData[digit][index]) + "\n"
-             elsif w == "VERTICAL"
-                vertical_segment(lcdDisplayData[digit][index]) + "\n"
-             end
+    individual_digits = digit.digits.reverse
+    e = individual_digits.map{ |i| form_digits(i)}
+    e.join("")
+  end
+
+  def form_digits(individual_digit)
+    lcdStates.each_with_index.map do |w, index|
+      if w ==  "HORIZONTAL"
+        horizontal_segment(lcdDisplayData[individual_digit][index]) + "\n"
+      elsif w == "VERTICAL"
+        vertical_segment(lcdDisplayData[individual_digit][index]) + "\n"
+      end
     end
-      e.join("")
   end
 
   def horizontal_segment(type)
@@ -35,13 +47,15 @@ class LCD
    def vertical_segment(type)
       case type
       when 1
-        return " "+ "|"
+        return " "+ " | "
       when 2
         return "|_" + " "
       when 3
         return "| |"
       when 4
         return "|_|"
+      when 5
+        return " _|"
       else
         return ""
       end
@@ -51,18 +65,5 @@ end
 
 
 
-  #   elsif digit == 6
-  #     <<~SQUIGGLY_HEREDOC
-  #        _
-  #       |_
-  #       |_|
-  #     SQUIGGLY_HEREDOC
-  #   elsif digit == 8
-  #   <<~SQUIGGLY_HEREDOC
-  #        _
-  #       |_|
-  #       |_|
-  #   SQUIGGLY_HEREDOC
-  #   end
-  # end
+
 
