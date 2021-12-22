9bb017c9f196086641f4a92a55a744e610334562
Bargavi19 <88387622+Bargavi19@users.noreply.github.com>
2021-12-22 14:17:16 +0000

    Method returns single digit (#68)
    * method returns digits 0,6,8
    
    * visualised 0 with horizontal vertical segments
    
    * renders a 0 test passed
    
    * renders a 6 test passed
    
    * renders a 8 test passed

--- lcd/lib/lcd.rb
+++ lcd/lib/lcd.rb
@@ -1,5 +1,68 @@
 # frozen_string_literal: true
 
 class LCD
-  # Write your code in here
+  attr_reader :digit, :lcdStates, :lcdDisplayData
+  def initialize(number)
+    @digit = number
+    @lcdStates = %w[HORIZONTAL VERTICAL VERTICAL]
+    @lcdDisplayData = {
+      0 => [1, 3, 4],
+      6 => [1, 2, 4],
+      8 => [1, 4, 4]
+    }
+  end
+
+  def render
+    e = lcdStates.each_with_index.map do |w, index|
+             if w ==  "HORIZONTAL"
+                horizontal_segment(lcdDisplayData[digit][index]) + "\n"
+             elsif w == "VERTICAL"
+                vertical_segment(lcdDisplayData[digit][index]) + "\n"
+             end
+    end
+      e.join("")
+  end
+
+  def horizontal_segment(type)
+      case type
+      when 1
+        return  " _ "
+      else
+        return ""
+      end
+   end
+
+   def vertical_segment(type)
+      case type
+      when 1
+        return " "+ "|"
+      when 2
+        return "|_" + " "
+      when 3
+        return "| |"
+      when 4
+        return "|_|"
+      else
+        return ""
+      end
+   end
 end
+
+
+
+
+  #   elsif digit == 6
+  #     <<~SQUIGGLY_HEREDOC
+  #        _
+  #       |_
+  #       |_|
+  #     SQUIGGLY_HEREDOC
+  #   elsif digit == 8
+  #   <<~SQUIGGLY_HEREDOC
+  #        _
+  #       |_|
+  #       |_|
+  #   SQUIGGLY_HEREDOC
+  #   end
+  # end
+
