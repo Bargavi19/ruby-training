012fc09fda3fa3ef7219cd358e3d0445d7286bf7
Bargavi19 <88387622+Bargavi19@users.noreply.github.com>
2021-12-24 23:15:05 +0000

    Viewing multiple digits (#69)
    * visualised digits from 0 to 9
    
    * grouped digits using transpose method
    
    * grouped multiple digits using transpose method

--- lcd/lib/lcd.rb
+++ lcd/lib/lcd.rb
@@ -7,20 +7,36 @@ class LCD
     @lcdStates = %w[HORIZONTAL VERTICAL VERTICAL]
     @lcdDisplayData = {
       0 => [1, 3, 4],
+      1 => [0, 1, 1],
+      2 => [1, 5, 2],
+      3 => [1, 5, 5],
+      4 => [0, 4, 1],
+      5 => [1, 2, 5],
       6 => [1, 2, 4],
-      8 => [1, 4, 4]
+      7 => [1, 1, 1],
+      8 => [1, 4, 4],
+      9 => [1, 4, 5]
     }
   end
 
   def render
-    e = lcdStates.each_with_index.map do |w, index|
-             if w ==  "HORIZONTAL"
-                horizontal_segment(lcdDisplayData[digit][index]) + "\n"
-             elsif w == "VERTICAL"
-                vertical_segment(lcdDisplayData[digit][index]) + "\n"
-             end
+    individual_digits = digit.digits.reverse
+    e = individual_digits.map{ |i| form_digits(i)}
+    if individual_digits.length == 1
+    e.transpose.join("\n") + "\n"
+    else
+     e.transpose.map { |element|  element.push("\n") }.join("")
     end
-      e.join("")
+  end
+
+  def form_digits(individual_digit)
+    lcdStates.each_with_index.map do |w, index|
+      if w ==  "HORIZONTAL"
+        horizontal_segment(lcdDisplayData[individual_digit][index])
+      elsif w == "VERTICAL"
+        vertical_segment(lcdDisplayData[individual_digit][index])
+      end
+    end
   end
 
   def horizontal_segment(type)
@@ -28,22 +44,24 @@ class LCD
       when 1
         return  " _ "
       else
-        return ""
+        return "   "
       end
    end
 
    def vertical_segment(type)
       case type
       when 1
-        return " "+ "|"
+        return " "+ " |"
       when 2
         return "|_" + " "
       when 3
         return "| |"
       when 4
         return "|_|"
+      when 5
+        return " _|"
       else
-        return ""
+        return " "
       end
    end
 end
@@ -51,18 +69,5 @@ end
 
 
 
-  #   elsif digit == 6
-  #     <<~SQUIGGLY_HEREDOC
-  #        _
-  #       |_
-  #       |_|
-  #     SQUIGGLY_HEREDOC
-  #   elsif digit == 8
-  #   <<~SQUIGGLY_HEREDOC
-  #        _
-  #       |_|
-  #       |_|
-  #   SQUIGGLY_HEREDOC
-  #   end
-  # end
+
 
