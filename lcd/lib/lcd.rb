e74f26966c3cd2423d602b72a25b926416177428
Bargavi19 <bargavi.muthusubramanian@globacap.com>
2021-12-25 21:02:44 +0000

    refactored_method_variable_names

--- lcd/lib/lcd.rb
+++ lcd/lib/lcd.rb
@@ -24,15 +24,15 @@ class LCD
 
   def render
     if num_or_string.is_a? Numeric
-     individual_digits_or_chars = num_or_string.digits.reverse
+     individual_number_or_chars = num_or_string.digits.reverse
     else
-      individual_digits_or_chars = num_or_string.chars
+      individual_number_or_chars = num_or_string.chars
     end
-    e = individual_digits_or_chars.map{ |i| display_lcd_digits_chars(i) }
-    e.transpose.map { |lcd_digits|  lcd_digits.push("\n") }.join("")
+    e = individual_number_or_chars.map{ |i| display_lcd_digits_or_chars(i) }
+    e.transpose.map { |lcd_digits_or_chars|  lcd_digits_or_chars.push("\n") }.join("")
   end
 
-  def display_lcd_digits_chars(individual_digit)
+  def display_lcd_digits_or_chars(individual_digit)
     lcdStates.map.with_index do |w, index|
       if w ==  "HORIZONTAL"
         horizontal_segment(lcdDisplayData[individual_digit][index])
