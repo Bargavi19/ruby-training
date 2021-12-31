f512cc7d732cd120691e158ba92bc670f0d81683
Bargavi19 <bargavi.muthusubramanian@globacap.com>
2021-12-31 17:44:34 +0000

    added the parameter for width

--- lcd/lib/lcd.rb
+++ lcd/lib/lcd.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class LCD
-  attr_reader :num_or_string, :lcdStates, :lcdDisplayData
-  def initialize(number_or_string)
+  attr_reader :num_or_string, :lcdStates, :lcdDisplayData, :size
+  def initialize(number_or_string, width = 1)
     @num_or_string = number_or_string
+    @size = width
     @lcdStates = %w[HORIZONTAL VERTICAL VERTICAL]
     @lcdDisplayData = {
       0 => [1, 3, 4],
@@ -48,26 +49,26 @@ class LCD
   def horizontal_segment(type)
       case type
       when 1
-        return  " _ "
+        return  "_" * size[:width].to_i + " "
       else
-        return "   "
+        return "   "* size[:width].to_i
       end
    end
 
    def vertical_segment(type)
       case type
       when 1
-        return " "+ " |"
+        return " "+ " |" * size[:width].to_i
       when 2
-        return "|_" + " "
+        return "|_" + " " * size[:width].to_i
       when 3
-        return "| |"
+        return "| |" * size[:width].to_i
       when 4
-        return "|_|"
+        return "|_|" * size[:width].to_i
       when 5
-        return " _|"
+        return " _|" * size[:width].to_i
       else
-        return "|  "
+        return "|  " * size[:width].to_i
       end
    end
 end
