cce26c26fad15cca43bc49fbb9d96790a8351e8e
Bargavi19 <bargavi.muthusubramanian@globacap.com>
2022-01-08 17:23:34 +0000

    removed reduntant code

--- lcd/lib/lcd.rb
+++ lcd/lib/lcd.rb
@@ -1,48 +1,44 @@
 # frozen_string_literal: true
 
 class LCD
-  attr_reader :num_or_string, :lcdStates, :lcdDisplayData, :no_of_lines
+  attr_reader :number_or_string, :width, :height
+
   def initialize(number_or_string, width: 1, height: 1)
-    @num_or_string = number_or_string
-    @no_of_lines = [width, height]
-    @lcdStates = %w[HORIZONTAL VERTICAL VERTICAL]
-    @lcdDisplayData = {
-      0 => [1, 3, 4],
-      1 => [0, 1, 1],
-      2 => [1, 5, 2],
-      3 => [1, 5, 5],
-      4 => [0, 4, 1],
-      5 => [1, 2, 5],
-      6 => [1, 2, 4],
-      7 => [1, 1, 1],
-      8 => [1, 4, 4],
-      9 => [1, 4, 5],
-      "a" => [1, 4, 3],
-      "c" => [1, 6, 2],
-      "b" => [0, 2, 4],
-      "f" => [1, 2, 6],
-      "e" => [1, 2, 2],
-      "d" => [0, 5, 4]
-    }
-    print no_of_lines
+    @number_or_string = number_or_string
+    @width = width
+    @height = height
   end
 
   def render
-    if num_or_string.is_a? Numeric
-     individual_number_or_chars = num_or_string.digits.reverse
+    if number_or_string.is_a? (Numeric)
+      individual_number_or_chars = number_or_string.digits.reverse
     else
-      individual_number_or_chars = num_or_string.chars
+      individual_number_or_chars = number_or_string.chars
     end
-    e = individual_number_or_chars.map{ |i| display_lcd_digits_or_chars(i) }
-    f = e.transpose.map do |lcd_digits_or_chars|
-      if @no_of_lines[0] != 1
-      lcd_digits_or_chars.push("\n")
-      end
-    end
-    f.join("")
+    individual_digit_display = individual_number_or_chars.map { |i| display_lcd_digits_or_chars(i) }
+    individual_digit_display.transpose.map { |lcd_digits_or_chars| lcd_digits_or_chars.push("\n") }.join("")
   end
 
   def display_lcd_digits_or_chars(individual_digit_or_char)
+    lcdStates = %w[HORIZONTAL VERTICAL HORIZONTAL VERTICAL]
+    lcdDisplayData = {
+      0 => [1, 3, 0, 4],
+      1 => [0, 1, 0, 1],
+      2 => [1, 5, 0, 2],
+      3 => [1, 5, 0, 5],
+      4 => [0, 4, 0, 1],
+      5 => [1, 2, 0, 5],
+      6 => [1, 2, 0, 4],
+      7 => [1, 1, 0, 1],
+      8 => [1, 4, 0, 4],
+      9 => [1, 4, 0, 5],
+      "a" => [1, 4, 0, 3],
+      "c" => [1, 6, 0, 2],
+      "b" => [0, 2, 0, 4],
+      "f" => [1, 2, 0, 6],
+      "e" => [1, 2, 0, 2],
+      "d" => [0, 5, 0, 4]
+    }
     lcdStates.map.with_index do |w, index|
       if w ==  "HORIZONTAL"
         horizontal_segment(lcdDisplayData[individual_digit_or_char][index])
@@ -52,36 +48,29 @@ class LCD
     end
   end
 
-  def horizontal_segment(type)
+
+    def horizontal_segment(type)
       case type
       when 1
-        return " " + "_" * no_of_lines[0] + " "
+        " " + "_"+ " "
       else
-        return " "* no_of_lines[1] if no_of_lines[1] != 1
-        return " "* no_of_lines[0] + "  "
-        return "   "
+        " "
       end
-   end
+    end
 
    def vertical_segment(type)
       case type
       when 1
-        return " "* no_of_lines[0] + " |" * no_of_lines[1]
-        return "  |\n" * (no_of_lines[1] - 1) + "  |"
+        " |"
       when 2
-        return "|_" + " " * no_of_lines[0]
+        "|"
       when 3
-        return "| |"
-      when 4
-        return "|" * no_of_lines[1] + "_" * no_of_lines[0]+ "|" * no_of_lines[1]
-        return "| |\n" * (no_of_lines[1] - 1) + "|_|"
+        "| |"
       when 5
-        return " " + "_" * no_of_lines[0]+ "|"
-      else
-        return "|  " * no_of_lines[0]
+        " " + "|"
       end
    end
-end
+ end
 
 
 
