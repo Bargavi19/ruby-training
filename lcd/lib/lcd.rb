5dd0085c2f9b97cb2ebe87b2bf68fbfbefe8b299
Bargavi19 <88387622+Bargavi19@users.noreply.github.com>
2022-01-09 20:51:00 +0000

    Renering 0 (#77)
    * added extra state and method
    
    * renders as 0 test passed

--- lcd/lib/lcd.rb
+++ lcd/lib/lcd.rb
@@ -10,68 +10,76 @@ class LCD
   end
 
   def render
-    if number_or_string.is_a? (Numeric)
-      individual_number_or_letter = number_or_string.digits.reverse
+    individual_number_or_letter.map { |individual_number_or_letter| display_lcd_digits_or_chars(individual_number_or_letter) }.join
+  end
+
+  def individual_number_or_letter
+    if number_or_string.is_a?(Numeric)
+      number_or_string.digits.reverse
     else
-      individual_number_or_letter = number_or_string.chars
+      number_or_string.chars
     end
-    individual_digit_display = individual_number_or_letter.map { |individual_number_or_letter| display_lcd_digits_or_chars(individual_number_or_letter) }
-    individual_digit_display.transpose.join("")
   end
 
   def display_lcd_digits_or_chars(individual_digit_or_char)
-    lcd_states = %w[HORIZONTAL VERTICAL HORIZONTAL VERTICAL HORIZONTAL VERTICAL]
-    lcd_display_data = {
-      0 => [1, 2, 0, 1, 1, 1],
-      1 => [0, 1, 0, 1],
-      2 => [1, 5, 0, 2],
-      3 => [1, 5, 0, 5],
-      4 => [0, 4, 0, 1],
-      5 => [1, 2, 0, 5],
-      6 => [1, 2, 0, 4],
-      7 => [1, 1, 0, 1],
-      8 => [1, 4, 0, 4],
-      9 => [1, 4, 0, 5],
-      "a" => [1, 4, 0, 3],
-      "c" => [1, 6, 0, 2],
-      "b" => [0, 2, 0, 4],
-      "f" => [1, 2, 0, 6],
-      "e" => [1, 2, 0, 2],
-      "d" => [0, 5, 0, 4]
-    }
-    lcd_states.map.with_index do |w, index|
-      if w ==  "HORIZONTAL"
-        if index == 0
-           " " + horizontal_segment(lcd_display_data[individual_digit_or_char][index]) + " "
-        else
-           horizontal_segment(lcd_display_data[individual_digit_or_char][index])
-        end
+    lcd_states = %w[line_one line_two line_three]
+    lcd_states.map.with_index do |line_number, index|
+      if line_number == "line_one"
+        " " + horizontal_segment(lcd_display_data[individual_digit_or_char][index]) + " " + "\n"
       else
-        vertical_segment(lcd_display_data[individual_digit_or_char][index])
+        remaining_segments(line_number, individual_digit_or_char, index)
       end
     end
   end
 
-
-    def horizontal_segment(type)
-      case type
-      when 1
-        "_"
-      else
-        ""
-      end
+  def remaining_segments(line_number, digit, segment)
+    if line_number == "line_two"
+      vertical_segment(lcd_display_data[digit][segment]) + horizontal_segment(lcd_display_data[digit][segment + 1]) + vertical_segment(lcd_display_data[digit][segment + 2]) + "\n"
+    else
+      vertical_segment(lcd_display_data[digit][segment + 2]) + horizontal_segment(lcd_display_data[digit][segment + 3]) + vertical_segment(lcd_display_data[digit][segment + 4]) + "\n"
     end
+  end
 
-   def vertical_segment(type)
-      case type
-      when 1
-        "|"
-      when 2
-        "| |"
-      else
-        " " + "|"
-      end
-   end
+  def horizontal_segment(type)
+    case type
+    when 1
+      "_"
+    else
+      " "
+    end
+  end
+
+ def vertical_segment(type)
+    case type
+    when 1
+      "|"
+    else
+      " "
+    end
+ end
+
+  def lcd_display_data
+    {
+    0 => [1, 1, 0, 1, 1, 1, 1],
+    1 => [0, 1, 0, 1],
+    2 => [1, 5, 0, 2],
+    3 => [1, 5, 0, 5],
+    4 => [0, 4, 0, 1],
+    5 => [1, 2, 0, 5],
+    6 => [1, 2, 0, 4],
+    7 => [1, 1, 0, 1],
+    8 => [1, 4, 0, 4],
+    9 => [1, 4, 0, 5],
+    "a" => [1, 4, 0, 3],
+    "c" => [1, 6, 0, 2],
+    "b" => [0, 2, 0, 4],
+    "f" => [1, 2, 0, 6],
+    "e" => [1, 2, 0, 2],
+    "d" => [0, 5, 0, 4]
+    }
+  end
+
+
  end
 
 
