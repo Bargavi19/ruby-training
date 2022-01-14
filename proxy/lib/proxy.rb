3db1dae250c04dec17a03a3f461062a73a5f7097
Bargavi19 <bargavi.muthusubramanian@globacap.com>
2022-01-14 19:15:37 +0000

    records messages

--- proxy/lib/proxy.rb
+++ proxy/lib/proxy.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 class Proxy
+  attr_reader :messages
   def initialize( proxy_object )
     @proxy_object = proxy_object
+    @messages = []
   end
 
   def method_missing(method_name, *args, &block)
     if @proxy_object.respond_to?(method_name)
+      messages.push(method_name)
       @proxy_object.send(method_name, *args, &block)
     else
       super(method_name, *args, &block)
