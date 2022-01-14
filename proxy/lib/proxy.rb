8ac77ae713089cb762397a1510fa23c78b909e27
Bargavi19 <88387622+Bargavi19@users.noreply.github.com>
2022-01-14 19:22:38 +0000

    Proxy 2 (#84)
    * operates TV power button
    
    * records messages
    
    * reports method that are called

--- proxy/lib/proxy.rb
+++ proxy/lib/proxy.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 class Proxy
+  attr_reader :messages
   def initialize( proxy_object )
     @proxy_object = proxy_object
+    @messages = []
   end
 
   def method_missing(method_name, *args, &block)
     if @proxy_object.respond_to?(method_name)
+      messages.push(method_name)
       @proxy_object.send(method_name, *args, &block)
     else
       super(method_name, *args, &block)
@@ -14,11 +17,16 @@ class Proxy
   end
 
   def respond_to?(method_name, include_private = false)
-    if method_name == @proxy_object.methods
+    proxy_method_names = @proxy_object.methods - Object.methods
+    if proxy_method_names.include?(method_name)
       true
     else
       super(method_name, include_private)
     end
+  end
+
+  def called?(method_name)
+    messages.include?(method_name)
   end
 
 end
